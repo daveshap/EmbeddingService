335a70f5ec1330bd395da85752e6dce582031ec4
David Shapiro <daveshap37@gmail.com>
2022-07-28 09:51:49 -0400

    removing string from list

--- USEv5_microservice.py
+++ USEv5_microservice.py
@@ -15,12 +15,11 @@ def home():
     payload = request.json  # payload should be like ["asdfasdf","asdfasdf"]
     print(payload)
     embeddings = embed(payload)
-    result = [{'vector':str(i.numpy().tolist()),'string':j} for i,j in zip(embeddings, payload)]
+    result = [{'vector':i.numpy().tolist(),'string':j} for i,j in zip(embeddings, payload)]
     return flask.Response(json.dumps(result), mimetype='application/json')
 
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=443, ssl_context='adhoc')
     #embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
     embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder-large/5")  # USEv5 is about 100x faster than 4
     app.run(host='0.0.0.0', port=999)
